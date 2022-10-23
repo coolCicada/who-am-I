9ac90140ddd5022c27ab00876af4d5cd0e4ff030
coolCicada <1475395755@qq.com>
2022-10-23 18:58:08 +0800

    feat:添加社交主页

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="app">
-    <div class="title">
-      <el-button icon="el-icon-search" circle></el-button>
-    </div>
+    <div class="header"></div>
     <div class="main">
       <div class="name">
+        <img src="@/assets/WechatIMG125.jpeg" alt="">
+      </div>
+      <div class="info">
+        <div class="title">社交主页</div>
+        <ul>
+          <li>
+            <a target="_blank" href="https://juejin.cn/user/835284566556014"><img src="@/assets/juejin.png" alt=""></a>
+          </li>
+          <li>
+            <a target="_blank" href="https://leetcode.cn/u/lcls/"><img src="@/assets/liko.png" alt=""></a>
+          </li>
+          <li>
+            <a target="_blank" href="https://github.com/coolCicada"><img src="@/assets/github.png" alt=""></a>
+          </li>
+        </ul>
       </div>
     </div>
     <div class="footer">
@@ -19,66 +32,94 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+<script setup lang="ts">
 
-@Component({
-  components: {},
-})
-export default class App extends Vue {}
 </script>
 
 <style lang="scss">
 html, body {
-  height: 100%;
   margin: 0;
   padding: 0;
   background: white;
 }
-body {
-  min-height: 500px;
-}
+
 #app {
   box-sizing: border-box;
+  min-height: 100vh;
   padding-bottom: 200px;
   height: 100%;
   position: relative;
-}
-.main {
-  width: 100%;
-  height: 100%;
-  position: relative;
-  
-  .name {
-    width: 200px;
-    height: 100px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    margin: auto;
-    line-height: 100px;
-    font-size: 5em;
-    text-align: center;
+  .header {
+    height: 20px;
+    margin-bottom: 25px;
   }
-}
-.footer {
-  height: 200px;
-  position: absolute;
-  bottom: 0;
-  background: gainsboro;
-  width: 100%;
+  .main {
+    width: 100%;
+    position: relative;
+    
+    .name {
+      height: 300px;
+      width: 300px;
+      margin: auto;
+      border-radius: 50%;
+      box-sizing: border-box;
+      padding: 10px 0;
+      background-color:antiquewhite;
 
-  .sign {
+      img {
+        display: block;
+        height: 280px;
+        margin: auto;
+        border-radius: 50%;
+      }
+    }
+
+    .info {
+      .title {
+        margin: 20px auto;
+        text-align: center;
+      }
+
+      ul {
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        li {
+          list-style: none;
+          width: 100px;
+          height: 100px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          margin: 0 20px;
+
+          img {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+  .footer {
+    height: 200px;
     position: absolute;
-    bottom: 0px;
-    left: 0px;
-    right: 0px;
-    text-align: center;
-    font-size: 1em;
-    text-decoration: none;
-    color: whitesmoke;
+    bottom: 0;
+    background: black;
+    width: 100%;
+
+    .sign {
+      position: absolute;
+      bottom: 0px;
+      left: 0px;
+      right: 0px;
+      text-align: center;
+      font-size: 1em;
+      text-decoration: none;
+      color: whitesmoke;
+    }
   }
 }
+
 </style>
