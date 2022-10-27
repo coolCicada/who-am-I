dcbb43cf71fcf8fe212c16f778688b1f359fc8b6
coolCicada <1475395755@qq.com>
2022-10-27 08:45:26 +0800

    feat:footer组件提取

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="footer">
+    <a
+      class="sign"
+      target="_blank"
+      href="https://beian.miit.gov.cn/"
+    >
+        京ICP备19043673号-2
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.footer {
+    height: 200px;
+    position: absolute;
+    bottom: 0;
+    background: black;
+    width: 100%;
+
+    .sign {
+      position: absolute;
+      bottom: 0px;
+      left: 0px;
+      right: 0px;
+      text-align: center;
+      font-size: 1em;
+      text-decoration: none;
+      color: whitesmoke;
+    }
+  }
+</style>
